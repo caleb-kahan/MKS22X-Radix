4049fa5c319e4eea79b88b87be105385eb1dba1a
Caleb Kahan <ckahan00@stuy.edu>
2019-03-31 16:22:24 -0400

    fixed up generic stuff and nested it

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,4 +1,36 @@
 public class MyLinkedList<E>{
+  public class Node{
+    private Node next,prev;
+    private E data;
+
+    Node next(){
+      return next;
+    }
+    public Node(Node next, Node prev, E data){
+      this.next = next;
+      this.prev = prev;
+      this.data = data;
+    }
+    Node prev(){
+      return prev;
+    }
+    void setNext(Node other){
+      next = other;
+    }
+    void setPrev(Node other){
+      prev = other;
+    }
+    E getData(){
+      return data;
+    }
+    E setData(E i){
+      data = i;
+    }
+    String toString(){
+      return "Previous Node: " + prev.data + ", This Node: " + data + ", Next Node: " + next.data + ".";
+    }
+  }
+
   private Node start;
   private Node end;
   private int length;
