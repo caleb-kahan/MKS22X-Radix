23a3ca40db8a6bec40363d0ae5de86c338d9a2a3
Caleb Kahan <ckahan00@stuy.edu>
2019-04-30 23:05:37 -0400

    empty lists work

--- CalebRadix.java
+++ CalebRadix.java
@@ -2,7 +2,10 @@ public class CalebRadix{
   public static void main(String [] args){
     int [] a = {1,1,2,-3,1,-5,1,1,1,1};
     Radix.radixsort(a);
+    int [] b = {};
+    Radix.radixsort(b);
     print(a);
+    print(b);
   }
   public static void print(int [] data){
     String returner = "{";
