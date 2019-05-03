9917b3a48bc508f146b8e4ed3f51b954c2dd687a
Caleb Kahan <ckahan00@stuy.edu>
2019-05-03 03:35:25 +0000

    im 1000% percent this works

--- CalebRadix.java
+++ CalebRadix.java
@@ -1,11 +1,23 @@
 public class CalebRadix{
   public static void main(String [] args){
     int [] a = {1,-1,21,-3,1,2,32,16,19,-5,1,-11,1,1,1};
+    int [] b = {-10,-9,-8,-7,-6,-5,-3,-2,-1,0,1,2,3,4,5};
     Radix.radixsort(a);
-    int [] b = {};
+    int [] c = {1};
+    int [] d = {0,2,-1};
+    int [] e = {0};
+    int [] f = {};
     Radix.radixsort(b);
+    Radix.radixsort(c);
+    Radix.radixsort(d);
+    Radix.radixsort(e);
+    Radix.radixsort(f);
     print(a);
     print(b);
+    print(c);
+    print(d);
+    print(e);
+    print(f);
   }
   public static void print(int [] data){
     String returner = "{";
